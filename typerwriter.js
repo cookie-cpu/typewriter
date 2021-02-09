c2d20f6b182240f6ee08535fc3da2ffc46ee58be
Matthew Cook <matthewcook13987@gmail.com>
2021-02-09 20:23:11 +0000

    changed the setTimeout function to increment it's delay after each loop so the characters print one after another.

--- typerwriter.js
+++ typerwriter.js
@@ -1,1 +1,12 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
+let timeOut = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, timeOut);
+  timeOut += 150;
+
+
+ 
+}
